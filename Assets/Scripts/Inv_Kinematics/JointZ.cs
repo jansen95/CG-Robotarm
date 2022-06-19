1b87e213603e382170db0441e79eb15dd7231882
Alex <alexander.jansen1995@gmail.com>
2022-06-19 23:21:07 +0200

    First success in joint limits

--- Assets/Scripts/Inv_Kinematics/JointZ.cs
+++ Assets/Scripts/Inv_Kinematics/JointZ.cs
@@ -5,7 +5,9 @@ namespace Inv_Kinematics
     public class JointZ : MonoBehaviour
     {
         public JointZ child;
-
+        public float minRotation;
+        public float maxRotation;
+        
         public JointZ GetChild()
         {
             return child;
@@ -14,6 +16,28 @@ namespace Inv_Kinematics
         public void Rotate(float angle)
         {
             transform.Rotate(Vector3.forward * angle);
+            Vector3 currentRotation = transform.localRotation.eulerAngles;
+            currentRotation.z = Mathf.Clamp(ConvertToAngle180(currentRotation.z), minRotation, maxRotation);
+            transform.localRotation = Quaternion.Euler (currentRotation);
+        }
+        
+        public static float ConvertToAngle180(float input)
+        {       
+            while (input > 360)
+            {
+                input = input - 360;
+            } 
+            while (input < -360)
+            {
+                input = input + 360;
+            }
+            if (input > 180)
+            {
+                input = input - 360;        
+            }
+            if (input < -180)
+                input = 360+ input;
+            return input;
         }
     }
 }
