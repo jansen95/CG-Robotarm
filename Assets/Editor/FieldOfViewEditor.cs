569c05b90be4bc59ac6157d7f78a0eef0c26935f
Alex <alexander.jansen1995@gmail.com>
2022-06-29 19:59:02 +0200

    deleted unnecessary scripts and warnings

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -1,33 +1,43 @@
+using Camera_Sensor;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(FieldOfView))]
-public class FieldOfViewEditor : Editor
+namespace Editor
 {
-    private void OnSceneGUI()
+    [CustomEditor(typeof(FieldOfView))]
+    public class FieldOfViewEditor : UnityEditor.Editor
     {
-        FieldOfView fov = (FieldOfView)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
-
-        Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
-        Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
-
-        Handles.color = Color.yellow;
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.radius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.radius);
-
-        if (fov.canSeePlayer)
+        private void OnSceneGUI()
         {
-            Handles.color = Color.green;
-            Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+            FieldOfView fov = (FieldOfView)target;
+            Handles.color = Color.white;
+            Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+
+
+            var eulerAngles = fov.transform.eulerAngles;
+            Vector3 viewAngle01 = DirectionFromAngle(eulerAngles.y, -fov.angle / 2);
+            Vector3 viewAngle02 = DirectionFromAngle(eulerAngles.y, fov.angle / 2);
+
+            Handles.color = Color.yellow;
+            var transform = fov.transform;
+            var position = transform.position;
+            Handles.DrawLine(position, position + viewAngle01 * fov.radius);
+            var transform1 = fov.transform;
+            var position1 = transform1.position;
+            Handles.DrawLine(position1, position1 + viewAngle02 * fov.radius);
+
+            if (fov.canSeePlayer)
+            {
+                Handles.color = Color.green;
+                Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+            }
         }
-    }
 
-    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
-    {
-        angleInDegrees += eulerY;
+        private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+        {
+            angleInDegrees += eulerY;
 
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
     }
 }
--- Assets/Scripts/Camera Sensor/FieldOfView.cs
+++ Assets/Scripts/Camera Sensor/FieldOfView.cs
@@ -1,60 +1,61 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldOfView : MonoBehaviour
+namespace Camera_Sensor
 {
-    public float radius;
-    [Range(0,360)]
-    public float angle;
-
-    public GameObject playerRef;
-
-    public LayerMask targetMask;
-    public LayerMask obstructionMask;
-
-    public bool canSeePlayer;
-
-    private void Start()
+    public class FieldOfView : MonoBehaviour
     {
-        playerRef = GameObject.FindGameObjectWithTag("Player");
-        StartCoroutine(FOVRoutine());
-    }
+        public float radius;
+        [Range(0,360)]
+        public float angle;
 
-    private IEnumerator FOVRoutine()
-    {
-        WaitForSeconds wait = new WaitForSeconds(0.2f);
+        public GameObject playerRef;
 
-        while (true)
+        public LayerMask targetMask;
+        public LayerMask obstructionMask;
+
+        public bool canSeePlayer;
+
+        private void Start()
         {
-            yield return wait;
-            FieldOfViewCheck();
+            playerRef = GameObject.FindGameObjectWithTag("Player");
+            StartCoroutine(FOVRoutine());
         }
-    }
 
-    private void FieldOfViewCheck()
-    {
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
-
-        if (rangeChecks.Length != 0)
+        private IEnumerator FOVRoutine()
         {
-            Transform target = rangeChecks[0].transform;
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            WaitForSeconds wait = new WaitForSeconds(0.2f);
 
-            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            while (true)
             {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                yield return wait;
+                FieldOfViewCheck();
+            }
+        }
 
-                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
-                    canSeePlayer = true;
+        private void FieldOfViewCheck()
+        {
+            Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+            if (rangeChecks.Length != 0)
+            {
+                Transform target = rangeChecks[0].transform;
+                Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+                if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+                {
+                    float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                    if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                        canSeePlayer = true;
+                    else
+                        canSeePlayer = false;
+                }
                 else
                     canSeePlayer = false;
             }
-            else
+            else if (canSeePlayer)
                 canSeePlayer = false;
         }
-        else if (canSeePlayer)
-            canSeePlayer = false;
     }
 }
